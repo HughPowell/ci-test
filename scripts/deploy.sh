7a57311193b96dd30dd577971eb02e520ae04962
Hugh Aristotle Powell <hugh@hughpowell.co.uk>
2017-05-17 23:24:40 +0100

    Debugging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo "Fakey password $LEIN_PASSWORD"
 git remote set-url origin https://$GITHUB_TOKEN@github.com/$GITHUB_ORG/ci-test.git &&
 git branch --set-upstream-to origin/master master &&
 git config user.name "$RELEASERS_NAME" &&

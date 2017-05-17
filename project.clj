b10afb8d05b37b179b38fa0650c4457923c250cd
Hugh Aristotle Powell <hugh@hughpowell.co.uk>
2017-05-17 19:20:30 +0100

    Don't sign releases

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :repositories [["releases" {:url "https://clojars.org/repo/"
                               :username :env
-                              :password :env}]]
+                              :password :env}
+                  :sign-releases false]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]

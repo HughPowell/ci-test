01bfa16e00749c57a57c1f6392dea7e01b00405b
Hugh Aristotle Powell <hugh@hughpowell.co.uk>
2017-05-24 23:20:59 +0100

    Make sure we're on the master branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,7 @@ if ! [[ $(git log -1 --pretty=%B) =~ ^Version\ [0-9]+\.[0-9]+\.[0-9]-SNAPSHOT$ ]
 	git config user.name "$RELEASERS_NAME" &&
 	git config user.email "$RELEASERS_EMAIL" &&
 	git config push.default simple &&
+	git checkout master &&
+	git pull origin master &&
 	lein release :patch
 fi

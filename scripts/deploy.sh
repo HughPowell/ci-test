2b0d2910b48ab4f6e97aa32ba7d5264723d025cc
Hugh Aristotle Powell <hugh@hughpowell.co.uk>
2017-05-24 23:02:13 +0100

    Add push setting

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-git remote set-url origin https://$GITHUB_TOKEN@github.com/$GITHUB_ORG/ci-test.git &&
-git branch --set-upstream-to origin/master master &&
-git config user.name "$RELEASERS_NAME" &&
-git config user.email "$RELEASERS_EMAIL" &&
-lein release :patch
+
+if ! [[ $(git log 1 --pretty=%B) =~ ^Version\ [0-9]+\.[0-9]+\.[0-9]-SNAPSHOT$ ]] ; then
+	git remote set-url origin https://$GITHUB_TOKEN@github.com/$GITHUB_ORG/ci-test.git &&
+	git branch --set-upstream-to origin/master master &&
+	git config user.name "$RELEASERS_NAME" &&
+	git config user.email "$RELEASERS_EMAIL" &&
+	git config push.default simple &&
+	lein release :patch
+fi

1fd22d841504d4c8af833363e186794739f23041
Hugh Powell <hugh@hughpowell.co.uk>
2017-05-25 17:54:28 +0000

    Version 0.1.17

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ci-test "0.1.17-SNAPSHOT"
+(defproject ci-test "0.1.17"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"

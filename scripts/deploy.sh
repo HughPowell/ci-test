ebb04e06c62cfec66b157741d27e49dd2efdedeb
Hugh Powell <hugh@hughpowell.co.uk>
2017-05-25 12:45:23 +0100

    Fix regex

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-if ! [[ $(git log -1 --pretty=%B) =~ ^Version\ [0-9]+\.[0-9]+\.[0-9]-SNAPSHOT$ ]] ; then
+if ! [[ $(git log -1 --pretty=%B) =~ ^Version\ [0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT$ ]] ; then
+
 	git remote set-url origin https://$GITHUB_TOKEN@github.com/$GITHUB_ORG/ci-test.git &&
 	git branch --set-upstream-to origin/master master &&
 	git config user.name "$RELEASERS_NAME" &&

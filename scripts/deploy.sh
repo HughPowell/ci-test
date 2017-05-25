ff7d5fcf4ac13829686d50e81f5c0852854600ce
Hugh Aristotle Powell <hugh@hughpowell.co.uk>
2017-05-25 18:53:02 +0100

    Display whether delpoyment is happening or not

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if ! [[ $(git log -1 --pretty=%B) =~ ^Version\ [0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT$ ]] ; then
-
+	echo "Deploying latest commit ..."
 	git remote set-url origin https://$GITHUB_TOKEN@github.com/$GITHUB_ORG/ci-test.git &&
 	git branch --set-upstream-to origin/master master &&
 	git config user.name "$RELEASERS_NAME" &&
@@ -10,4 +10,6 @@ if ! [[ $(git log -1 --pretty=%B) =~ ^Version\ [0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT$ 
 	git checkout master &&
 	git pull origin master &&
 	lein release :patch
+else
+	echo "Last commit created new snapshot, no deployment required."
 fi
